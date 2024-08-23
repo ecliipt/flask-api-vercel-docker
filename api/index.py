e3a4578aafcec17684bd472aad965637ea113591
Eclipt <137305099+ecliipt@users.noreply.github.com>
2024-08-23 23:43:20 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -150,6 +150,3 @@ def create_user():
     user_data = request.get_json()
     response = asyncio.run(get_response(query=user_data['input']))
     return jsonify(response), 201
-
-if __name__ == "__main__":
-    app.run()
